c45b7366da03da314b8223655339c4860b504e03
Faelixx <sasha.taylor7@gmail.com>
2023-11-19 10:32:56 -0700

    Feat: Edge case errors handled, invalid breed, url error

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,19 @@
 const request = require('request');
-const fs = require('fs');
 
 const processArgs = process.argv;
 const url = processArgs[2];
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${url}`, (error, message, body) => {
-  const data = JSON.parse(body);
-  // console.log(data)
-  // console.log(typeof data);
-  console.log(data[0].description);
-})
+  if (!error && message.statusCode === 200) {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log("Please enter a valid cat breed.");
+    } else {
+      console.log(data[0].description);
+    }
+  }
+  if (error) {
+    console.log("Received the following error: ", error);
+  }
+});
 
