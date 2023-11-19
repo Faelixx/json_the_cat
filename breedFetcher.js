d5006b7be825e0f1017f148490c9944650406a30
Faelixx <sasha.taylor7@gmail.com>
2023-11-19 10:12:57 -0700

    Feat: User search for breed description using cmd line args.

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,9 +4,10 @@ const fs = require('fs');
 const processArgs = process.argv;
 const url = processArgs[2];
 
-request(url, (error, message, body) => {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${url}`, (error, message, body) => {
   const data = JSON.parse(body);
   // console.log(data)
   // console.log(typeof data);
+  console.log(data[0].description);
 })
 
